64134cd9bf2b07e1db9b7046db9c22fc00587b8d
Live session user <ubuntu@ubuntu.(none)>
2013-10-15 11:35:02 +0000

    My first git commit

--- NIM.cpp
+++ NIM.cpp
@@ -40,6 +40,7 @@ int main( )
 		cout << "                      2. Birta reglurnar i NIM" << endl;
 		cout << "                      3. Haetta " << endl << endl;
 		cout << "                      Veldu 1, 2 eda 3:" << endl << endl;
+	        cout << " thordis " << endl << endl;
 		cin >> val;
 		switch ( val )
 		{
